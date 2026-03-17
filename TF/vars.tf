caf6f6b71bfe03598550e4078cb61398b221e7c0
TechWMarcus <amarricaden42@gmail.com>
2026-03-16 19:10:25 -0600

    added a linter

--- TF/vars.tf
+++ TF/vars.tf
@@ -7,12 +7,3 @@ variable "region" {
   type = string
   default = "us-west-1"
   }
-variable "ami_id" {
-  type = string
-  default = "ami-0290e60ec230db1e4"
-  
-}
-variable "subnets" {
-  type = list(string)
-  default = ["lb-subnet", "jumpbox-subnet"]
-}
