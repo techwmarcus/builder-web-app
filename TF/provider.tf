34e56c3a7970233e1bc36e839bf5af00e1be2173
TechWMarcus <amarricaden42@gmail.com>
2026-03-16 19:15:26 -0600

    added a linter

--- TF/provider.tf
+++ TF/provider.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = "1.14.7" 
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
--- TF/vars.tf
+++ TF/vars.tf
@@ -2,8 +2,3 @@ data "aws_availability_zones" "available" {
   state = "available"  
 }
 
-
-variable "region" {
-  type = string
-  default = "us-west-1"
-  }
